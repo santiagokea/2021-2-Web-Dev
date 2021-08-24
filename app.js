9cf4c7faa84ebfcc07b6a530b18e4d9e7ef2b34b
Santiago Donoso <sand@kea.dk>
2021-08-24 15:23:17 +0200

    ...

---
+++ app.js
@@ -0,0 +1,12 @@
+var first_name = "Santiago"
+var last_name = "Donoso"
+// Create a variable with your last name
+// show this in the console
+// Hi, Santiago Donoso
+// console.log("Hi, Santiago Donoso")
+// follow by
+// console.log("Hi, "+first_name+" "+last_name)
+console.log("Hi,",first_name,last_name)
+console.log('Hi,',first_name,last_name)
+
+
