2df48f4d19f1aa96bb78a06064f2369b45f1f801
Santiago Donoso <sand@kea.dk>
2021-08-24 15:56:09 +0200

    ...

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// a is array
+// j is json
+// s is string/text
+// i is number
+//               0    1    2
+var aLetters = ["a", "b", "c", "d", "e"]
+// push is adding
+aLetters.push("d") //["a", "b", "c", "d"]
+// pop you delete from the end
+aLetters.pop()
+aLetters.pop()
+aLetters.pop()
+console.log(aLetters)
+// console.log(aLetters[2])
+// Arrays goes hand-by-hand with loops
+
+
+
+
+
+
+
 // var name = "Santiago"
 // document.querySelector("section").style.display = "none" 
 // document.querySelector("section").innerHTML = name
@@ -5,7 +27,7 @@
 // document.querySelector("nav div").style.color = "red"
 // document.querySelector("nav#slogan").style.color = "red"
 // document.querySelector("nav").children[1].style.color = "red"
-document.querySelector("nav div:nth-of-type(1)").style.color = "red"
+// document.querySelector("nav div:nth-of-type(1)").style.color = "red"
 
 // var first_name = "Santiago"
 // var last_name = "Donoso"
