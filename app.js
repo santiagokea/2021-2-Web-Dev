873ca0d86d7255999c0ca726a129b24c845ed2f4
Santiago Donoso <sand@kea.dk>
2021-08-24 15:34:10 +0200

    ...

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-var first_name = "Santiago"
-var last_name = "Donoso"
+var name = "Santiago"
+// document.querySelector("section").style.display = "none" 
+document.querySelector("section").innerHTML = name
+
+
+
+
+// var first_name = "Santiago"
+// var last_name = "Donoso"
 // Create a variable with your last name
 // show this in the console
 // Hi, Santiago Donoso
 // console.log("Hi, Santiago Donoso")
 // follow by
 // console.log("Hi, "+first_name+" "+last_name)
-console.log("Hi,",first_name,last_name)
-console.log('Hi,',first_name,last_name)
-
+// console.log("Hi,",first_name,last_name)
+// console.log('Hi,',first_name,last_name)
+// String literal or template literal
+// ' ""  ``
+// console.log(`Hi, ${first_name} ${last_name}`)
 
